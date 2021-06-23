4032e1c84dfbd57cd8fff675cf65fa63fc688077
Salih <bayraktar.salih@hotmail.com>
2021-06-23 11:11:23 +0300

    Main Menu If Replaced With Switch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-import Characters.Players.*;
-import Locations.BattleLocations.*;
-import Locations.NormalLocations.*;
+import Characters.Players.Archer;
+import Characters.Players.Knight;
+import Characters.Players.Player;
+import Characters.Players.Samurai;
+import Locations.BattleLocations.Cave;
+import Locations.BattleLocations.Forest;
+import Locations.BattleLocations.Mine;
+import Locations.BattleLocations.River;
+import Locations.NormalLocations.Market;
+import Locations.NormalLocations.SafeHouse;
 
 import java.util.Map;
 import java.util.Scanner;
@@ -15,61 +22,43 @@ public class Main {
         System.out.println("Firstly choose your fighter (1 Samurai, 2 Archer, 3 Knight)");
         do {
             String query = scanner.nextLine();
-            if("1".equals(query)){
+            if ("1".equals(query)) {
                 player = new Samurai();
                 break;
             } else if ("2".equals(query)) {
                 player = new Archer();
                 break;
-            } else if("3".equals(query)) {
+            } else if ("3".equals(query)) {
                 player = new Knight();
                 break;
             } else {
                 System.out.println("Invalid Fighter");
             }
-        }while (true);
+        } while (true);
 
-        System.out.printf("You choice %s. Your character traits are %s\n\n",player.getName(),player);
+        System.out.printf("You choice %s. Your character traits are %s\n\n", player.getName(), player);
 
-        do {
+
+        while (!player.isGameEnd()) {
             System.out.println("Where do you wanna go? (1 Safe House, 2 Market, 3 Cave, 4 Forest, 5 River, 6 Mine, 0 Check Win)");
             String query = scanner.nextLine();
-            if("1".equals(query)){
-
-                new SafeHouse(player);
-
-            } else if("2".equals(query)){
-
-                new Market(player);
-
-            } else if("3".equals(query)){
-
-                new Cave(player);
-
-            } else if("4".equals(query)){
-
-                new Forest(player);
-
-            } else if("5".equals(query)){
-
-                new River(player);
-
-            }else if("6".equals(query)){
-
-                new Mine(player);
-
-            } else if("0".equals(query)){
-
-                System.out.printf("Welcome back %s. Your character traits are %s\n\n",player.getName(),player);
-
-               for(Map.Entry<String,Boolean> entry : player.getInventory().getRequiredItems().entrySet()){
-                   if(!entry.getValue()){
-                       System.out.printf("You need %s for win. \n",entry.getKey());
-                   }
-               }
-
+            switch (query) {
+                case "1" -> new SafeHouse(player);
+                case "2" -> new Market(player);
+                case "3" -> new Cave(player);
+                case "4" -> new Forest(player);
+                case "5" -> new River(player);
+                case "6" -> new Mine(player);
+                case "0" -> {
+                    System.out.printf("Welcome back %s. Your character traits are %s\n\n", player.getName(), player);
+                    for (Map.Entry<String, Boolean> entry : player.getInventory().getRequiredItems().entrySet()) {
+                        if (!entry.getValue()) {
+                            System.out.printf("You need %s for win. \n", entry.getKey());
+                        }
+                    }
+                }
             }
-        }while (!player.isGameEnd());
+        }
 
         System.out.println("Congratulations you have collected all the items and finished the game");
 
